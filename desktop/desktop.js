d3d5b0248c16fef773d33c888c3c1e9c35eb81fb
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2021-06-18 18:01:03 -0300

    Add files via upload

---
+++ desktop/desktop.js
@@ -0,0 +1,62 @@
+var popup= document.querySelector(".popup_options")
+var popupwall= document.querySelector(".popup_wallpaper")
+var wallpaper= document.querySelector("body")
+var imagem= localStorage.getItem("wallpaper")
+wallpaper.style.backgroundImage= "url("+imagem+")"
+
+const um= "../imagens/fundo_desktop.jpeg"
+const dois= "../imagens/desktop1.jpeg"
+const tres= "../imagens/desktop2.png"
+
+
+function option(){
+    popup.style.display= "flex"
+}
+
+function desliga(){
+    popup.style.display= "none"
+    window.location= "../index.html"
+}
+
+function logout(){
+    alert("Isso é meramente decorativo, não serve pra nada KKKKK")
+    // localStorage.removeItem("personagem");
+    window.location="../index.html";
+}
+
+function cancela(){
+    popup.style.display= "none"
+}
+
+function mudar(img){
+        wallpaper.style.backgroundImage= "url("+img+")"
+
+        localStorage.setItem("wallpaper", img)
+}
+
+function fechar(){
+    popupwall.style.display= "none";
+}
+
+function astral(){
+    window.location="https://app.astraltabletop.com/play/-MT6eLOMx8fl5cpoGMW-/portal"
+}
+
+function livro(){
+    alert("Livro somente disponível em inglês :(");
+    window.location="../Livro_CoC.pdf";
+}
+
+document.onmousedown = click 
+
+function click(event) { 
+      
+    if (event.button == 2) { 
+              var confirmado= confirm("Deseja mudar o plano de fundo?")
+              if(confirmado==true){
+                  popupwall.style.display="block"
+              }else{
+
+              }
+    } 
+} 
