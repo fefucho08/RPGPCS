28f8fd9cde1c1a63014a56755ea12b61fc793bea
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2021-06-18 18:04:44 -0300

    Add files via upload

---
+++ anotacoes/carregando.js
@@ -0,0 +1,18 @@
+function valida(){
+    var senha= input_senha.value
+
+    if(senha=="H284fgwh618Ma3rC8s"){
+        window.location="./docs/kiki.html"
+    }else if(senha=="clebinho2000"){
+        window.location="./docs/lily.html"
+    }else if(senha=="c4l3bnanna"){
+        window.location="./docs/hannah.html"
+    }else if(senha="£u@n@c0//1n5"){
+        window.location="./docs/luana.html"
+    }else if(senha=="smith1102gabi"){
+        window.location="./docs/gabi.html"
+    }else{
+        alert("Senha incorreta")
+    }
+
+}
