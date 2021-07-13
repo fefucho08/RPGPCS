74dad69f5ec01836a629b5756a5819d622dad060
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2021-07-13 13:33:15 -0300

    Update google.js

--- google/google.js
+++ google/google.js
@@ -1,6 +1,12 @@
 function pesquisa(){
     var pesquisado = search_area.value;
-    alert("Nenhum resultado encontrado")
 
+    if(pesquisado=="$5oR@"){
+        window.location="../escotilha/pc1/pc1.html"
+    }else if(pesquisado=="4$pNg"){
+        window.location="../imagens/Planos PMC.png"
+    }else{
+        alert("Nenhum resultado encontrado")
+    }
     search_area.value= "";
 }
